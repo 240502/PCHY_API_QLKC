e420da43b7d2db9ae42f64907856f77796bcef00
Nguyễn Văn Sang <sanghip200@gmail.com>
2024-11-25 09:13:03 +0700

    first commit

--- API_PCHY/obj/Debug/net5.0/API_PCHY.AssemblyInfo.cs
+++ API_PCHY/obj/Debug/net5.0/API_PCHY.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_PCHY")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea20735c8b083ceeca17737422124f6d55446bf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_PCHY")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_PCHY")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
